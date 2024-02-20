908e72d6c9a9d33c47919a58515961f9bc8abf74
Espen Sørby <espen@sorby.net>
2024-02-20 16:48:04 +0100

    Add styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,37 @@
-import './App.css';
+import { useCallback, useEffect, useState } from 'react';
+import axios, { AxiosResponse } from 'axios';
+import { RepoProps } from './types';
+import RepoShow from './components/RepoShow';
+import './app.scss';
 
 const App = () => {
+  const [repos, setRepos] = useState<RepoProps[]>([]);
+
+  const fetchRepos = useCallback(async () => {
+    try {
+      const response: AxiosResponse = await axios.get('https://api.github.com/search/repositories?q=language:javascript&sort=stars&order=desc&per_page=100');
+      setRepos(response.data.items);
+    } catch (error) {
+      throw new Error(`Failed to fetch repositories: ${error}`);
+    }
+  }, []);
+
+  useEffect(() => { 
+    fetchRepos(); 
+  }, [fetchRepos]);
 
   return (
-    <div>
-     Repo list
-    </div>
+    <>
+      <h1>The most popular JS libraries on Github</h1>
+      <div className='container'>
+        {repos.length !== 0 ? repos.map((repo: RepoProps) => (
+          <a className='link-wrapper' href={repo.html_url} target="blank" key={repo.id}>
+            <RepoShow repo={repo} />
+          </a>
+        )) : <p>Fetching repos...</p>}
+      </div>
+    </>
   )
 }
 
-export default App
+export default App;
